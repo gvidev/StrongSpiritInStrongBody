1be6bdda9fa6fd8204fc329534e2db3c1484a37c
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2024-05-21 23:58:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+/*
+Project name Default (Template) Project
+Created by georgi on date 21.5.2024 г.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
